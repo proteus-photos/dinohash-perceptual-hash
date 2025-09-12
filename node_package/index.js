3d872e828f77b60341d45dce79ad9a5c61db9d2a
Daniel Tehrani <contact@dantehrani.com>
2025-09-11 20:02:48 -0700

    javascript interface to run multiple models

--- node_package/index.js
+++ node_package/index.js
@@ -1,9 +1,12 @@
 const downloader = require('./src/downloader');
 const model = require('./src/model');
 const inference = require('./src/inference');
+const utils = require("./src/utils");
 
 module.exports = {
   downloadModel: downloader.downloadModel,
   loadModel: model.loadModel,
-  hash: inference.hash
+  hash: inference.hash,
+  getModelUrl: utils.getModelUrl,
+  getModels: utils.getModels
 };
