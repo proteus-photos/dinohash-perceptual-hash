edb2ae473d3ea5bfe064f7e53b26c74c99423a39
Daniel Tehrani <contact@dantehrani.com>
2025-09-13 14:40:31 -0700

    fix

--- node_package/src/utils.js
+++ node_package/src/utils.js
@@ -1,31 +1,31 @@
 
 const MODEL_URLS = {
-  "ResNet-101-Efficient-B0_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-101–Efficient-B0_scripted.onnx",
-  "ResNet-101–Efficient-B1_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-101–Efficient-B1_scripted.onnx",
-  "ResNet-101–MobileNet-v3_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-101–MobileNet-v3_scripted.onnx",
-  "ResNet-101–ResNet-18_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-101–ResNet-18_scripted.onnx",
-  "ResNet-101–ResNet-34_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-101–ResNet-34_scripted.onnx",
-  "ResNet-152–Efficient-B0_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-152–Efficient-B0_scripted.onnx",
-  "ResNet-152–Efficient-B1_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-152–Efficient-B1_scripted.onnx",
-  "ResNet-152–MobileNet-v3_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-152–MobileNet-v3_scripted.onnx",
-  "ResNet-152–ResNet-18_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-152–ResNet-18_scripted.onnx",
-  "ResNet-152–ResNet-34_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-152–ResNet-34_scripted.onnx",
-  "ResNet-50*2–Efficient-B0_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50*2–Efficient-B0_scripted.onnx",
-  "ResNet-50*2–Efficient-B1_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50*2–Efficient-B1_scripted.onnx",
-  "ResNet-50*2–MobileNet-v3_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50*2–MobileNet-v3_scripted.onnx",
-  "ResNet-50*2–ResNet-18_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50*2–ResNet-18_scripted.onnx",
-  "ResNet-50*2–ResNet-34_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50*2–ResNet-34_scripted.onnx",
-  "ResNet-50–Efficient-B0_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50–Efficient-B0_scripted.onnx",
-  "ResNet-50–Efficient-B1_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50–Efficient-B1_scripted.onnx",
-  "ResNet-50–MobileNet-v3_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50–MobileNet-v3_scripted.onnx",
-  "ResNet-50–ResNet-18_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50–ResNet-18_scripted.onnx",
-  "ResNet-50–ResNet-34_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50–ResNet-34_scripted.onnx",
+  "ResNet-101-Efficient-B0_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-101-Efficient-B0_scripted.onnx",
+  "ResNet-101-Efficient-B1_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-101-Efficient-B1_scripted.onnx",
+  "ResNet-101-MobileNet-v3_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-101-MobileNet-v3_scripted.onnx",
+  "ResNet-101-ResNet-18_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-101-ResNet-18_scripted.onnx",
+  "ResNet-101-ResNet-34_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-101-ResNet-34_scripted.onnx",
+  "ResNet-152-Efficient-B0_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-152-Efficient-B0_scripted.onnx",
+  "ResNet-152-Efficient-B1_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-152-Efficient-B1_scripted.onnx",
+  "ResNet-152-MobileNet-v3_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-152-MobileNet-v3_scripted.onnx",
+  "ResNet-152-ResNet-18_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-152-ResNet-18_scripted.onnx",
+  "ResNet-152-ResNet-34_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-152-ResNet-34_scripted.onnx",
+  "ResNet-50*2-Efficient-B0_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50*2-Efficient-B0_scripted.onnx",
+  "ResNet-50*2-Efficient-B1_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50*2-Efficient-B1_scripted.onnx",
+  "ResNet-50*2-MobileNet-v3_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50*2-MobileNet-v3_scripted.onnx",
+  "ResNet-50*2-ResNet-18_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50*2-ResNet-18_scripted.onnx",
+  "ResNet-50*2-ResNet-34_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50*2-ResNet-34_scripted.onnx",
+  "ResNet-50-Efficient-B0_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50-Efficient-B0_scripted.onnx",
+  "ResNet-50-Efficient-B1_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50-Efficient-B1_scripted.onnx",
+  "ResNet-50-MobileNet-v3_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50-MobileNet-v3_scripted.onnx",
+  "ResNet-50-ResNet-18_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50-ResNet-18_scripted.onnx",
+  "ResNet-50-ResNet-34_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-50-ResNet-34_scripted.onnx",
   "dino_mnv2_sslight_traced": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/dino_mnv2_sslight_traced.onnx",
   "dino_vitt16_sslight_traced": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/dino_vitt16_sslight_traced.onnx",
   "moco_mnv2_sslight_traced": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/moco_mnv2_sslight_traced.onnx",
   "swav_mnv2_sslight_traced": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/swav_mnv2_sslight_traced.onnx",
-  "ResNet-101–Efficient-B0_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-101–Efficient-B0_scripted.onnx",
-  "dino_r18_sslight_scripted":"https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/dino_r18_sslight_scripted.onnx",
+  "ResNet-101-Efficient-B0_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/ResNet-101-Efficient-B0_scripted.onnx",
+  "dino_r18_sslight_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/dino_r18_sslight_scripted.onnx",
   "dino_r34_sslight_scripted": "https://huggingface.co/dantehrani/proteus-models/resolve/main/onnx/dino_r34_sslight_scripted.onnx"
 }
 
@@ -34,11 +34,11 @@ const getModels = () => {
 }
 
 const getModelUrl = (modelName) => {
-    if (Object.keys(MODEL_URLS).includes(!modelName)) {
-        throw new Error(`Model ${modelName} not found`)
-    }
+  if (Object.keys(MODEL_URLS).includes(!modelName)) {
+    throw new Error(`Model ${modelName} not found`)
+  }
 
-    return MODEL_URLS[modelName]
+  return MODEL_URLS[modelName]
 }
 
 
