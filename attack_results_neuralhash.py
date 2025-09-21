1419a3953c2040468faf5b6b70c9bd3829edc58f
Daniel Tehrani <contact@dantehrani.com>
2025-09-20 20:03:18 -0700

    cleanup

--- attack_results_neuralhash.py
+++ attack_results_neuralhash.py
@@ -1,7 +1,4 @@
-
-import os
 import numpy as np
-import torch
 from PIL import Image
 import torchvision.transforms as T
 from tqdm import tqdm
@@ -9,7 +6,7 @@ from torch.utils.data import DataLoader, Dataset, Subset
 from apgd_attack import APGDAttack
 from paper_results.temp_neuralhash import NeuralHash
 import time
-from torchvision import datasets, transforms
+from torchvision import  transforms
 import onnx
 
 preprocess = lambda x: np.array(x.convert("RGB").resize([360, 360])).astype(np.float32) / 255.0
@@ -26,10 +23,11 @@ class ImageDataset(Dataset):
         return preprocess(image).transpose(2,0,1), self.image_files[idx]
 
 
+# Just loading one image for testing
 image_files = ["./test.png"]
 
 dataset = ImageDataset(image_files)
-dataloader = DataLoader(dataset, batch_size=10, shuffle=False)
+dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
 
 epsilon = 64
 
@@ -38,13 +36,17 @@ neuralhash = NeuralHash(onnx_model)
 
 apgd = APGDAttack(neuralhash, eps=epsilon, device="cpu")
 
-acc_hamming_distance = 0
-images_processed = 0
-
 accs = 0
 count = 0
 
+to_pil = transforms.ToPILImage()
+
 for image_tensors, labels in tqdm(dataloader):
+    # Save the image before the attack for reference
+    image_before_attack = image_tensors[0]
+    to_pil(image_before_attack).save(f"image_before_attack.png")
+
+    # Forward pass
     forward_start = time.time()
     logits = neuralhash.forward(image_tensors)
     forward_end = time.time()
@@ -53,23 +55,23 @@ for image_tensors, labels in tqdm(dataloader):
     adv_images, _ = apgd.attack_single_run(image_tensors, logits, n_iter=10)
     attack_end = time.time()
 
-    first_attack_image = adv_images[0]
-    to_pil = transforms.ToPILImage()
-    img = to_pil(first_attack_image)
-    img.save(f"first_attack_image2.png")
+    # Save the image after the attack for reference
+    image_after_attack = adv_images[0] 
+    to_pil(image_after_attack).save(f"image_after_attack.png")
 
     adv_logits = neuralhash.forward(adv_images, c=20, logits=True)
 
     adv_logits_bits = (adv_logits[0]>=0).flatten()
     logits_bits = (logits[0]>=0).flatten()
 
+    # Print the output hashes for reference
     adv_logits_str = "".join(['1' if b else '0' for b in adv_logits_bits])
     logits_str = "".join(['1' if b else '0' for b in logits_bits])
     print(adv_logits_str)
     print(logits_str)
 
     diff = (adv_logits_bits != logits_bits).sum()
-    print(f"Diff: {diff}")
+    print(f"Hamming distance: {diff}")
 
     acc = ((adv_logits>=0) == (logits>=0)).float().mean(1).cpu().numpy()
     accs += acc.sum().item()
